c066f9e8b984d27d6950293818afa8e3713ed9de
aestdile@gmail.com <aestdile@gmail.com>
2025-05-03 11:34:36 +0500

    AdminService: Update Profile Of Admin

--- EPAMSYSTEM/Services/AdminService.cs
+++ EPAMSYSTEM/Services/AdminService.cs
@@ -1313,6 +1313,190 @@ namespace EPAMSYSTEM.Services
         }
 
 
+        /*----------------Update Profile of Admin----------------*/
+
+        public string UpdateProfileOfAdmin()
+        {
+            Console.Clear();
+            Console.WriteLine("\n-----------------Update Profile of Admin------------------\n");
+
+            Console.Write("Your Id: ");
+            string idInput = Console.ReadLine();
+
+            Guid id;
+            while (true)
+            {
+                if (string.IsNullOrWhiteSpace(idInput))
+                {
+                    Console.Write("Id can not be empty. Try again: ");
+                }
+                else if (!Guid.TryParse(idInput, out id))
+                {
+                    Console.Write("Id must be a valid GUID. Try again: ");
+                }
+                else
+                {
+                    break;
+                }
+                idInput = Console.ReadLine();
+            }
+
+            Console.Write("Enter First Name: ");
+            string firstName = Console.ReadLine();
+
+            while (true)
+            {
+                if (string.IsNullOrWhiteSpace(firstName))
+                {
+                    Console.Write("First name cannot be empty. Try again: ");
+                }
+                else if (firstName.Length < 3)
+                {
+                    Console.Write("First name must be at least 3 characters long. Try again: ");
+                }
+                else if (firstName.Length > 40)
+                {
+                    Console.Write("First name must be less than 40 characters long. Try again: ");
+                }
+                else if (!char.IsUpper(firstName[0]))
+                {
+                    Console.Write("First name must start with an uppercase letter. Try again: ");
+                }
+                else if (!firstName.All(c => char.IsLetter(c)))
+                {
+                    Console.Write("First Name must contain only letters. Try again: ");
+                }
+                else
+                {
+                    break;
+                }
+                firstName = Console.ReadLine();
+            }
+
+
+            Console.Write("Enter Last Name: ");
+            string lastName = Console.ReadLine();
+
+            while (true)
+            {
+                if (string.IsNullOrWhiteSpace(lastName))
+                {
+                    Console.Write("Last name cannot be empty. Try again: ");
+                }
+                else if (lastName.Length < 4)
+                {
+                    Console.Write("Last name must be at least 3 characters long. Try again: ");
+                }
+                else if (lastName.Length > 40)
+                {
+                    Console.Write("Last name must be less than 40 characters long. Try again: ");
+                }
+                else if (!char.IsUpper(lastName[0]))
+                {
+                    Console.Write("Last name must start with an uppercase letter. Try again: ");
+                }
+                else if (!lastName.All(c => char.IsLetter(c)))
+                {
+                    Console.Write("Last Name must contain only letters. Try again: ");
+                }
+                else
+                {
+                    break;
+                }
+                lastName = Console.ReadLine();
+            }
+
+
+            Console.Write("Enter Email: ");
+            string email = Console.ReadLine();
+
+            while (true)
+            {
+                if (string.IsNullOrWhiteSpace(email))
+                {
+                    Console.Write("Email cannot be empty. Try again: ");
+                }
+                else if (email.Length < 5)
+                {
+                    Console.Write("Email must be at least 5 characters long. Try again: ");
+                }
+                else if (email.Length > 40)
+                {
+                    Console.Write("Email must be less than 40 characters long. Try again: ");
+                }
+                else if (!email.Contains("@"))
+                {
+                    Console.Write("Email must contain '@'. Try again: ");
+                }
+                else
+                {
+                    break;
+                }
+                email = Console.ReadLine();
+            }
+
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+            while (true)
+            {
+                if (string.IsNullOrWhiteSpace(password))
+                {
+                    Console.Write("Password can not be empty!");
+                }
+                else if (password.Length < 8)
+                {
+                    Console.Write("Password can not be less 8 characters!");
+                }
+                else if (!password.Any(char.IsUpper))
+                {
+                    Console.Write("Password must contain at least one uppercase letter!");
+                }
+                else if (!password.Any(char.IsLower))
+                {
+                    Console.Write("Password must contain at least one lowercase letter!");
+                }
+                else if (!password.Any(char.IsDigit))
+                {
+                    Console.Write("Password must contain at leat one digit!");
+                }
+                else if (!password.Any(ch => "!@#$%^&*()-_=+[{]};:'\",<.>/?".Contains(ch)))
+                {
+                    Console.Write("Password must contain at least one special character!");
+                }
+                else
+                {
+                    break;
+                }
+                password = Console.ReadLine();
+            }
+
+            List<Admin> admins = new List<Admin>();
+            if (System.IO.File.Exists(FilePath))
+            {
+                string json = System.IO.File.ReadAllText(FilePath);
+                admins = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Admin>>(json) ?? new List<Admin>();
+            }
+
+            Admin admin = admins.FirstOrDefault(e => e.Id == id);
+            if (admin == null)
+            {
+                return "Admin not found";
+            }
+
+            admin.FirstName = firstName;
+            admin.LastName = lastName;
+            admin.Email = email;
+            admin.Password = password;
+            string jsonOutput = Newtonsoft.Json.JsonConvert.SerializeObject(admins, Newtonsoft.Json.Formatting.Indented);
+            System.IO.File.WriteAllText(FilePath, jsonOutput);
+
+
+            return "Admin Profile updated successfully!";
+        }
+
+
+
 
     }
 }
