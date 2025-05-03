02757b8dcc78994b463d8095ac5816862cc2f09f
aestdile@gmail.com <aestdile@gmail.com>
2025-05-03 10:07:22 +0500

    Models: created Admin

--- EPAMSYSTEM/Models/Admin.cs
+++ EPAMSYSTEM/Models/Admin.cs
@@ -1,6 +1,25 @@
-﻿namespace EPAMSYSTEM.Models
+﻿
+
+using System;
+
+namespace EPAMSYSTEM.Models
 {
-    class Admin
+    public class Admin
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Department { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime DateOfJoiningToCompany { get; set; }
+        public int Attendance { get; set; }
+        public TimeSpan StartWorkTime { get; set; }
+        public TimeSpan EndWorkTime { get; set; }
+        public bool MonthlyTask { get; set; }
+        public string NotificationsOfAdmin { get; set; }
+        public string NotificationsOfEmployee { get; set; }
     }
 }
